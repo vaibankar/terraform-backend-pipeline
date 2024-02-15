2c0d660d4e45c9b07d4568236df4bf9979924211
vaibankar <113019542+vaibankar@users.noreply.github.com>
2024-02-16 00:33:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,30 @@
+#create s3 bucket
 
+resource "aws_s3_bucket" "mybucket" {
+    bucket = "zomato_bucket"
+    versioning {
+      enabled = true
+    }
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+  
+}
+
+
+#create dynamodb table
+
+resource "aws_dynamodb_table" "statelock" {
+  name             = "state-lock"
+  hash_key         = "LockId"
+  billing_mode     = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockId"
+    type = "S"
+  }
+}
