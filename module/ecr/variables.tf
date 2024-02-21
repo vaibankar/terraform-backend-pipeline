83d2cac139fa37339ecf44f7335cec331bc1d698
vaibankar <113019542+vaibankar@users.noreply.github.com>
2024-02-21 23:29:07 +0530

    Update variables.tf

--- module/ecr/variables.tf
+++ module/ecr/variables.tf
@@ -1,1 +1,33 @@
+variable "environment" {
+    description = "Deployment Environment"
+  
+}
+variable "project" {
+    description = "Project Environment"
+  
+}
+variable "ecr_name" {
+    description = "The name of the ecr registory"
+    type = string
+    default = " "
+  
+}
+variable "image_tag_mutability" {
+    description = "provide image mutability"
+    type = string
+    default = " "
+  
+}
+variable "encryption_type" {
+    description = "provide type to encryption here"
+    type = string
+    default = "KMS"
+  
+}
+variable "tags" {
+    description = "The key-value maps for tagging"
+    type = map(string)
+    
+  
+}
 
