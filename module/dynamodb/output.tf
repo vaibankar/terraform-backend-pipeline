77ba6fa5f9ad29c760098de005687692b3800485
vaibankar <113019542+vaibankar@users.noreply.github.com>
2024-02-21 23:31:17 +0530

    Update output.tf

--- module/dynamodb/output.tf
+++ module/dynamodb/output.tf
@@ -1,1 +1,12 @@
+output "table_name" {
+  value = aws_dynamodb_table.terraform_lock.name
+}
+
+output "hash_key" {
+  value = aws_dynamodb_table.terraform_lock.hash_key
+}
+
+output "billing_mode" {
+  value = aws_dynamodb_table.terraform_lock.billing_mode
+}
 
