b31b1f403d645fe2f3a50d2fbc39f8b3954a468d
vaibankar <113019542+vaibankar@users.noreply.github.com>
2024-02-21 23:30:38 +0530

    Update main.tf

--- module/dynamodb/main.tf
+++ module/dynamodb/main.tf
@@ -1,1 +1,10 @@
+resource "aws_dynamodb_table" "terraform_lock" {
+  name             = var.dynamodb_table_name
+  hash_key         = var.dynamodb_hash_key
+  billing_mode     = var.dynamodb_billing_mode
 
+  attribute {
+    name = "LockId"
+    type = "S"
+  }
+}
