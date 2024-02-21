9c5381032fd0204751ac904b7ef31654669fc625
vaibankar <113019542+vaibankar@users.noreply.github.com>
2024-02-21 23:14:24 +0530

    Update development.tf

--- development.tf
+++ development.tf
@@ -16,14 +16,14 @@ module "Myvpc" {
   availability_zones   = local.production_availability_zones
 
 }
-# module "zomato" {
-#   source                     = "./module/ec2"
-#   ami                        = var.ami
-#   instance_type              = var.instance_type
-#   key_name                   = var.key_name
-#   subnet_id                  = module.Myvpc.public_subnets_id[0]
-#   default_security_group_ids = module.Myvpc.default_security_group_ids
-# }
+ module "zomato" {
+   source                     = "./module/ec2"
+   ami                        = var.ami
+   instance_type              = var.instance_type
+   key_name                   = var.key_name
+   subnet_id                  = module.Myvpc.public_subnets_id[0]
+   default_security_group_ids = module.Myvpc.default_security_group_ids
+ }
 
 module "eks_cluster" {
   source          = "./module/eks"
@@ -43,5 +43,26 @@ module "eks_cluster" {
   min_size         = 1
 
   max_size         = 3
+}
+module "s3" {
+  source           = "./module/s3"
+  bucket_name      = var.bucket_name
+}
+
+module "dynamodb" {
+  source                        = "./module/dynamodb"
+  dynamodb_table_name           = var.dynamodb_table_name
+  dynamodb_hash_key             = var.dynamodb_hash_key
+  dynamodb_billing_mode         = var.dynamodb_billing_mode
+}
+module "ecr" {
+    source               = "./module/ecr"
+    environment          = var.environment
+    project              = var.project 
+    ecr_name             = var.ecr_name
+    tags                 = var.tags
+    image_tag_mutability = var.image_mutability
+
+  
 }
 
