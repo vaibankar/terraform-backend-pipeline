b43bcfd9248334ef55475db145b6ba823c112f92
vaibankar <113019542+vaibankar@users.noreply.github.com>
2024-02-21 23:28:45 +0530

    Update output.tf

--- module/ecr/output.tf
+++ module/ecr/output.tf
@@ -1,1 +1,20 @@
+output "ecr_repository_url" {
+  value = aws_ecr_repository.ecr.repository_url
+}
+
+output "ecr_repository_name" {
+  value = aws_ecr_repository.ecr.name
+}
+
+output "ecr_repository_encryption_type" {
+  value = aws_ecr_repository.ecr.encryption_configuration.0.encryption_type
+}
+
+output "ecr_repository_scan_on_push" {
+  value = aws_ecr_repository.ecr.image_scanning_configuration.0.scan_on_push
+}
+
+output "ecr_repository_tags" {
+  value = aws_ecr_repository.ecr.tags
+}
 
