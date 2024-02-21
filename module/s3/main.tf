f057cfe85808c89bda4a5598a849b7b9b588d0ec
vaibankar <113019542+vaibankar@users.noreply.github.com>
2024-02-21 23:23:39 +0530

    Update main.tf

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -1,1 +1,14 @@
-
+resource "aws_s3_bucket" "s3_bucket" {
+    bucket = var.bucket_name
+    versioning {
+      enabled = true
+    }
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+  
+}
