90de7b34e4c513bc05055f8a6ee63a1d37df655b
vaibankar <113019542+vaibankar@users.noreply.github.com>
2024-02-21 23:31:39 +0530

    Update variables.tf

--- module/dynamodb/variables.tf
+++ module/dynamodb/variables.tf
@@ -1,1 +1,18 @@
+variable "dynamodb_table_name" {
+  description = "The name of the DynamoDB table"
+  type        = string
+  
+}
+
+variable "dynamodb_hash_key" {
+  description = "The hash key of the DynamoDB table"
+  type        = string
+  
+}
+
+variable "dynamodb_billing_mode" {
+  description = "The billing mode for the DynamoDB table"
+  type        = string
+  
+}
 
