07e3ced1de361802b8c8430f8f9a7a0ca0a79f18
vaibankar <113019542+vaibankar@users.noreply.github.com>
2024-03-10 12:43:29 +0530

    Update output.tf

--- module/ec2/output.tf
+++ module/ec2/output.tf
@@ -1,13 +1,13 @@
 output "instance_id" {
-  value = aws_instance.zomato.id
+  value = aws_instance.Netflix.id
 }
 
 output "instance_public_ip" {
-  value = aws_instance.zomato.public_ip
+  value = aws_instance.Netflix.public_ip
 }
 
 output "instance_private_ip" {
-  value = aws_instance.zomato.private_ip
+  value = aws_instance.Netflix.private_ip
 }
 
 output "public_key" {
